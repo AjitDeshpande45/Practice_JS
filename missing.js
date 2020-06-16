8c4e44385f83c27696d6aa9dcb74117c29b1262c
Ajit <ajitdeshpande00.ad@gmail.com>
2020-06-16 12:04:10 +0530

    Missing Element

--- missing.js
+++ missing.js
@@ -1,36 +1,25 @@
-function missing(arr1, arr2, l1, l2) {
+function Missing_Element(arr1, arr2, l1, l2) {
 
-    if (l1 > l2) {
-        for (var i = 0; i < l1; i++) {
-            var f = 0;
-            for (var j = 0; j < l2; j++) {
-                if (arr1[i] == arr2[j]) {
-                    f = f + 1;
-                }
-            }
-            if (f == 0) {
-                console.log(arr1[i]);
+    for (let i = 0; i < l1; i++) {
+        var f = 0;
+        for (var j = 0; j < l2; j++) {
+            if (arr1[i] == arr2[j]) {
+                f = f + 1;
             }
         }
-    }
-    if (l1 < l2) {
-        for (var i = 0; i < l2; i++) {
-            var f = 0;
-            for (var j = 0; j < l1; j++) {
-                if (arr2[i] == arr1[j]) {
-                    f = f + 1;
-                }
-            }
-            if (f == 0) {
-                console.log(arr2[i]);
-            }
+        if (f == 0) {
+            console.log(arr1[i]);
         }
     }
+
 }
-
-
-var arr1 = [1, 4, 2, 5, 3];
-var arr2 = [5, 4, 1, 2];
-var l1 = arr1.length;
-var l2 = arr2.length;
-var x = missing(arr1, arr2, l1, l2);
+let arr1 = [5, 4, 1, 2];
+let arr2 = [1, 4, 2, 5, 3];
+let l1 = arr1.length;
+let l2 = arr2.length;
+if (l1 > l2) {
+    Missing_Element(arr1, arr2, l1, l2);
+}
+else {
+    Missing_Element(arr2, arr1, l2, l1);
+}
